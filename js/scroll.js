c168579398dc549f09af207ae64d83e491336833
Ben Cheng <benjamin.cheng90@gmail.com>
2015-12-12 15:42:32 -0500

    Fix scroll and sticky header

--- js/scroll.js
+++ js/scroll.js
@@ -4,7 +4,7 @@ $('document').ready(function(){
     $root.animate({
       //scrolltop value needs to something like
       // var targetOffset = $target.offset().top - 55;
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top-45
     }, 500);
     return false;
   });
